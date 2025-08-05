59e8e09cb16476febe943cf05e4ff2385eea8cf9
MinSung Jung <minpro22@naver.com>
2025-08-05 17:19:08 +0900

    25.08.05 JdbcTemplate 를 이용한 소스코드 커밋

--- src/test/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
@@ -46,8 +46,9 @@ public class MemberServiceIntegrationTest {
         //When
         memberService.join(member1);
         IllegalStateException e = assertThrows(IllegalStateException.class,
-                () -> memberService.join(member2));//예외가 발샐한다.
+                () -> memberService.join(member2));//예외가 발생해야 한다.
 
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
     }
+
 }
